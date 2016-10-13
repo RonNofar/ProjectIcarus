ee36087e5faf6090b7e61183ee09cc810443d2fb
RonNofar <ronnofar2@gmail.com>
2016-10-13 11:03:15 -0700

    UFO, rest of project

---
+++ Bird/Assets/AlienCameraControl.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienCameraControl : MonoBehaviour {
+
+    public Camera myCamera;
+
+    public float cameraThreshhold = 5; // in degrees
+    public float cameraTurnSpeed = 1;
+
+    private Transform myTransform;
+    private Rigidbody myRigidbody;
+    private Transform cameraTransform;
+
+    private Vector3 eulerAngles;
+    private Vector3 cameraEulerAngles;
+
+	// Use this for initialization
+	void Start () {
+        SetInitialReferences();
+
+    }
+
+    void SetInitialReferences ()
+    {
+        myTransform = GetComponent<Transform>();
+        myRigidbody = GetComponent<Rigidbody>();
+        cameraTransform = myCamera.GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (cameraEulerAngles.y > eulerAngles.y + cameraThreshhold) // add a camera threshhold
+        {
+            cameraEulerAngles.y -= cameraTurnSpeed;
+        }
+        else if (cameraEulerAngles.y < eulerAngles.y - cameraThreshhold)
+        {
+            cameraEulerAngles.y += cameraTurnSpeed;
+        }
+	}
+
+    void UpdateEulerAngles ()
+    {
+        eulerAngles = myTransform.eulerAngles;
+        cameraEulerAngles = cameraTransform.eulerAngles;
+        
+    }
+}
---
+++ Bird/Assets/AlienMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienMovement : MonoBehaviour {
+
+    public float movementSpeed = 1;
+
+
+    private Transform myTransform;
+    private Rigidbody myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+        SetInitialReference();
+
+    }
+
+    void SetInitialReference () {
+        myTransform = GetComponent<Transform>();
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float translation_vertical = Input.GetAxis("Vertical") * movementSpeed;
+        float translation_horizontal = Input.GetAxis("Horizontal") * movementSpeed;
+        translation_vertical *= Time.deltaTime;
+        translation_horizontal *= Time.deltaTime;
+        myRigidbody.AddForce(myTransform.forward * translation_vertical, ForceMode.Acceleration); // WASD forward/backward
+        myRigidbody.AddForce(myTransform.right * translation_horizontal, ForceMode.Acceleration);
+    }
+}
