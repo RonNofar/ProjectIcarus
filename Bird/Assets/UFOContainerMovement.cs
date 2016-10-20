9986cb041c6396e6079adb0c0b11164c93d5d3e9
Ron Nofar <Ron Nofar>
2016-10-20 10:38:43 -0700

    UFOTestLevel, plus forgotten daily commit 10/20/16

---
+++ Bird/Assets/UFOContainerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UFOContainerMovement : MonoBehaviour {
+
+    public float rotationSpeed = 1000;
+    public float movementSpeed = 10;
+
+    private Transform myTransform;
+    private Rigidbody myRigidbody;
+
+
+	// Use this for initialization
+	void Start () {
+        SetInitialReferences();
+	}
+
+    void SetInitialReferences () {
+        myTransform = GetComponent<Transform>();
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (myTransform != null) {
+            float rotation = Input.GetAxis("Mouse X") * rotationSpeed;
+            //myRigidbody.AddTorque(myTransform.right * rotation);
+            myTransform.Rotate(myTransform.forward * Time.deltaTime * rotation, Space.World);
+
+            myTransform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+        }
+	}
+}
--- Bird/Assets/_CompletedAssets/Scripts/Camera/CameraFollow.cs
+++ Bird/Assets/_CompletedAssets/Scripts/Camera/CameraFollow.cs
@@ -5,27 +5,30 @@ namespace CompleteProject
 {
     public class CameraFollow : MonoBehaviour
     {
-        public Transform target;            // The position that that camera will be following.
-        public float smoothing = 5f;        // The speed with which the camera will be following.
+        public GameObject targetObject;
+        private Transform targetTransform;            // The position that that camera will be following.
+        public float smoothing = 1f;        // The speed with which the camera will be following.
 
+        private Transform m_Transform;
 
         Vector3 offset;                     // The initial offset from the target.
 
 
-        void Start ()
-        {
+        void Start () {
             // Calculate the initial offset.
-            offset = transform.position - target.position;
+            m_Transform = transform;
+            if (targetObject != null) targetTransform = targetObject.transform;
+            else Debug.Log("No GameObject found.");
+            offset = m_Transform.position - targetTransform.position;
         }
 
 
-        void FixedUpdate ()
-        {
+        void FixedUpdate () {
             // Create a postion the camera is aiming for based on the offset from the target.
-            Vector3 targetCamPos = target.position + offset;
+            Vector3 targetCamPos = targetTransform.position + offset;
 
             // Smoothly interpolate between the camera's current position and it's target position.
-            transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+            m_Transform.position = Vector3.Lerp (m_Transform.position, targetCamPos, smoothing * Time.deltaTime);
         }
     }
 }
