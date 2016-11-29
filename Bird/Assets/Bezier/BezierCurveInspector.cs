73b5433b186924af1545cd8150df8888f93b6255
Ron Nofar <Ron Nofar>
2016-11-29 10:37:39 -0800

    BezierSpline and SplineInspector

--- Bird/Assets/Bezier/BezierCurveInspector.cs
+++ Bird/Assets/Bezier/BezierCurveInspector.cs
@@ -11,7 +11,9 @@ public class BezierCurveInspector : Editor {
 
     private const int lineSteps = 10;
 
-	private void OnSceneGUI () {
+    private const float directionScale = 0.5f;
+
+    private void OnSceneGUI () {
         curve = target as BezierCurve;
         handleTransform = curve.transform;
         handleRotation = Tools.pivotRotation == PivotRotation.Local ?
@@ -20,11 +22,16 @@ public class BezierCurveInspector : Editor {
         Vector3 p0 = ShowPoint(0);
         Vector3 p1 = ShowPoint(1);
         Vector3 p2 = ShowPoint(2);
+        Vector3 p3 = ShowPoint(3);
 
         Handles.color = Color.gray;
         Handles.DrawLine(p0, p1);
-        Handles.DrawLine(p1, p2);
+        Handles.DrawLine(p2, p3);
 
+        ShowDirections();
+        Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+
+        /*
         Handles.color = Color.white;
         Vector3 lineStart = curve.GetPoint(0f);
         Handles.color = Color.green;
@@ -36,7 +43,7 @@ public class BezierCurveInspector : Editor {
             Handles.color = Color.green;
             Handles.DrawLine(lineEnd, lineEnd + curve.GetDirection(i / (float)lineSteps));
             lineStart = lineEnd;
-        }
+        }*/
     }
 
     private Vector3 ShowPoint (int index) {
@@ -49,5 +56,15 @@ public class BezierCurveInspector : Editor {
             curve.points[index] = handleTransform.InverseTransformPoint(point);
         }
         return point;
+    }
+
+    private void ShowDirections () {
+        Handles.color = Color.green;
+        Vector3 point = curve.GetPoint(0f);
+        Handles.DrawLine(point, point + curve.GetDirection(0f) * directionScale);
+        for (int i = 1; i <= lineSteps; i++) {
+            point = curve.GetPoint(i / (float)lineSteps);
+            Handles.DrawLine(point, point + curve.GetDirection(i / (float)lineSteps) * directionScale);
+        }
     }
 }
