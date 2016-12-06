9812c6f79459c59b6a0d14ae113d33d125324e10
RonNofar <ronnofar2@gmail.com>
2016-12-05 16:03:28 -0800

    mainMenu scene added

---
+++ Bird/Assets/TitleImageAnimator.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class TitleImageAnimator : MonoBehaviour {
+
+    public Image titleImage;
+
+    float r;
+    float g;
+    float b;
+
+    float xScale;
+    float yScale;
+    float zScale;
+
+    Quaternion orgRot;
+
+    public float speed = 0.01f;
+    public float scaleSpeed = 0.1f;
+
+    // Use this for initialization
+    void Awake () {
+        r = 1;
+        g = 0;
+        b = 0;
+        Debug.Log("r: " + r + " | g: " + g + " | b: " + b);
+
+        xScale = titleImage.transform.localScale.x;
+        yScale = titleImage.transform.localScale.y;
+        zScale = titleImage.transform.localScale.z;
+        Debug.Log("xScale: " + xScale + " | yScale: " + yScale + " | zScale: " + zScale);
+
+        orgRot = titleImage.transform.localRotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (r >= 1 && g < 1 && b <= 0) {
+            r = 1;    g += speed; b = 0;
+        }
+        else if (r > 0 && g >= 1 && b <= 0) {
+            r -= speed; g = 1;    b = 0;
+        }
+        else if (r <= 0 && g >= 1 && b < 1) {
+            r = 0;      g = 1;    b += speed;
+        }
+        else if (r <= 0 && g > 0 && b >= 1) {
+            r = 0;      g -= speed; b = 1;
+        }
+        else if (r < 1 && g <= 0 && b >= 1) {
+            r += speed; g = 0;      b = 1;
+        }
+        else if (r >= 1 && g <= 0 && b > 0) {
+            r = 1;    g = 0;      b -= speed;
+        }
+        else Debug.Log("Something went wrong in RGB flow!");
+        //Debug.Log("r: " + r + " | g: " + g + " | b: " + b);
+        titleImage.color = new Color(r, g, b);
+
+        
+        float pi = Mathf.PI;
+        titleImage.transform.localScale = new Vector3(
+            (Mathf.Sin(Time.timeSinceLevelLoad * scaleSpeed) + 4f)/4f,//(Mathf.Sin(Mathf.PingPong(Time.timeSinceLevelLoad, pi * 2)) + 2f)/2,
+            (Mathf.Sin(Time.timeSinceLevelLoad * scaleSpeed) + 4f)/4f,//(Mathf.Sin(Mathf.PingPong(Time.timeSinceLevelLoad, pi * 2)) + 2f)/2,
+            zScale);
+        Debug.Log(Time.time+" | "+titleImage.transform.localScale);
+        /*
+        titleImage.transform.localRotation = new Quaternion(
+            Mathf.Sin(Mathf.PingPong(Time.time, pi * 2)),
+            Mathf.Sin(Mathf.PingPong(Time.time, pi * 2)),
+            orgRot.z,
+            orgRot.w
+            );*/
+        //titleImage.transform.localScale = new Vector2()
+    }
+     /*
+    public float MyPingPong (float t, float length, float speed=10) {
+        // speed is a ratio S.T. the smaller it is the faster this function will run
+        float result = t;
+        if (t<length)
+        {
+            t += length/speed;
+        } else if (t)
+        return result;
+    }*/
+}
